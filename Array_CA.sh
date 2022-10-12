c6bb714216d49af46e987ea5bedb41466506b6fd
Alya Mirza Safira <109938539+AlyaMirzaSafira@users.noreply.github.com>
2022-10-12 19:37:42 +0700

    Tugas_6
    Tugas_6

---
+++ Array_CA.sh
@@ -0,0 +1,9 @@
+# deklarasi array compound assignment
+distroLinuxDesktop=('BlankOn' 'Ubuntu' 'Debian' 'ArchLinux'  'LinuxMint')
+distroLinuxServer=('UbuntuServer' 'CentOS' 'Fedoraserver')
+
+# cara Mengambil Nilai Array
+echo ${distroLinuxDesktop[*]}
+echo ${distroLinuxServer[*]}
+
+
---
+++ Array_ID.sh
@@ -0,0 +1,14 @@
+distroLinuxDesktop[0]=BlankOn
+distroLinuxDesktop[1]=Ubuntu
+distroLinuxDesktop[2]=Debian
+distroLinuxDesktop[3]=ArchLinux
+distroLinuxDesktop[4]=Linuxmint
+
+distroLinuxServer[0]=UbuntuServer
+distroLinuxServer[1]=CentOS
+distroLinuxServer[2]=FedoraServer
+
+#Cara mengambil nilai array
+echo ${distroLinuxDesktop[*]}
+echo ${distroLinuxServer[*]}
+
